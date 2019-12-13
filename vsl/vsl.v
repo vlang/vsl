0df7fe21471115bc6799ca4338cb813472c83e6e
ulises-jeremias <ulisescf.24@gmail.com>
2019-12-12 23:18:47 -0300

    Added copyright to main file

--- vsl/vsl.v
+++ vsl/vsl.v
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 Ulises Jeremias Cornejo Fandos. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
 module vsl
 
 import vsl.quaternion
