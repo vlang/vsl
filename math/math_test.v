b4ee1c10d3417f3109cbc0722349d41ff093a14d
ulises-jeremias <ulisescf.24@gmail.com>
2020-02-02 19:45:47 -0300

    Fmt

--- math/math_test.v
+++ math/math_test.v
@@ -2073,7 +2073,7 @@ fn test_large_tan() {
 	for i := 0; i < vf_.len; i++ {
 		f1 := tan_large_[i]
 		f2 := tan(vf_[i] + large)
-                // TODO: improve trig reduction for large arguments
+		// TODO: improve trig reduction for large arguments
 		// assert soclose(f1, f2, 1e-9)
 	}
 }
