cac5c92a66c11814bf3b47d5a93b8c4e33586ace
ulises-jeremias <ulisescf.24@gmail.com>
2020-02-03 05:34:30 -0300

    Updated specfunc

---
+++ specfunc/specfunc.v
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 Ulises Jeremias Cornejo Fandos. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+module specfunc
+
+import vsl.math
+
+fn is_neg_int(x f64) bool {
+	if x < 0 {
+		_, xf := math.modf(x)
+		return xf == 0
+	}
+	return false
+}
