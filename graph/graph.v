08cc2ccbd59297dff8742387a9f20543ffe6fea0
ulises-jeremias <ulisescf.24@gmail.com>
2020-02-05 23:16:19 -0300

    Updated license years

--- graph/graph.v
+++ graph/graph.v
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Ulises Jeremias Cornejo Fandos. All rights reserved.
+// Copyright (c) 2019-2020 Ulises Jeremias Cornejo Fandos. All rights reserved.
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
 // Module graph implements solvers based on Graph theory
