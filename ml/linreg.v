b16fb3042cfa5ab43ff46e1b8584f0428d125580
ulises-jeremias <ulisescf.24@gmail.com>
2020-12-16 03:06:04 -0300

    Updated doc

--- ml/linreg.v
+++ ml/linreg.v
@@ -12,7 +12,7 @@ mut:
 	stat   &Stat      // statistics
 
 	// workspace
-	e []f64 // vector e = b⋅o + x⋅theta - y [nSamples]
+	e []f64 // vector e = b⋅o + x⋅theta - y [nb_samples]
 }
 
 // new_lin_reg returns a new LinReg object
