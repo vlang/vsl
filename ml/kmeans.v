603c0dedb914cde3a808f98988220221ab0432d3
ulises-jeremias <ulisescf.24@gmail.com>
2021-01-01 21:31:31 -0300

    Updated comments

--- ml/kmeans.v
+++ ml/kmeans.v
@@ -11,7 +11,7 @@ mut:
 	nb_classes int // expected number of classes
 	bins       &gm.Bins // "bins" to speed up searching for data points given their coordinates (2D or 3D only at the moment)
 pub mut:
-	classes    []int // [nSamples] indices of classes of each sample
+	classes    []int // [nb_samples] indices of classes of each sample
 	centroids  [][]f64 // [nb_classes][nb_features] coordinates of centroids
 	nb_members []int // [nb_classes] number of members in each class
 }
