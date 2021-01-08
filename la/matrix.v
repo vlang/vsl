d9393f73d301ce911eb7ac0999b00314fbcb5a42
ulises-jeremias <ulisescf.24@gmail.com>
2021-01-08 14:02:20 -0300

    Fix

--- la/matrix.v
+++ la/matrix.v
@@ -366,5 +366,5 @@ pub fn safe_print<T>(format string, message T) string {
 	buf := [byte(0)]
 	mut ptr := &buf[0]
 	unsafe { C.sprintf(charptr(ptr), charptr(format.str), message) }
-	return tos(buf.data, vstrlen(buf.data)).trim_space()
+	return tos(buf.data, vstrlen(buf.data))
 }
