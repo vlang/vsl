c562d0fac90ce85951639fbe5b0bc7946cb88681
ulises-jeremias <ulisescf.24@gmail.com>
2021-01-08 00:14:06 -0300

    Fix

--- la/blas/_flags.v
+++ la/blas/_flags.v
@@ -3,9 +3,9 @@
 // that can be found in the LICENSE file.
 module blas
 
-#flag linux -O2 -I/usr/local/include -I/usr/lib -I@VROOT/blas
+#flag linux -O2 -I/usr/local/include -I/usr/lib -I@VROOT/la/blas
 #flag linux -lopenblas -llapacke -L/usr/local/lib -L/usr/lib
 #flag windows -O2
 #flag windows -lopenblas -lgfortran
-#flag darwin -I/usr/local/opt/openblas/include -I@VROOT/blas
+#flag darwin -I/usr/local/opt/openblas/include -I@VROOT/la/blas
 #flag darwin -lopenblas -L/usr/local/opt/openblas/lib
