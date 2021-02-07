137b9cf06fbfe636d0bbe53c71371505ec3488d8
ulises-jeremias <ulisescf.24@gmail.com>
2021-02-07 13:07:37 -0300

    Fixed tests

--- util/mapops.v
+++ util/mapops.v
@@ -3,7 +3,7 @@ module util
 // int_ints_map_append appends a new item to a map of slice.
 // Note: this function creates a new slice in the map if key is not found.
 pub fn int_ints_map_append(o map[int][]int, key int, item int) map[int][]int {
-	mut m := o
+	mut m := o.clone()
 	if key in m {
 		mut slice := m[key]
 		slice << item
@@ -17,7 +17,7 @@ pub fn int_ints_map_append(o map[int][]int, key int, item int) map[int][]int {
 // str_ints_map_append appends a new item to a map of slice.
 // Note: this function creates a new slice in the map if key is not found.
 pub fn str_ints_map_append(o map[string][]int, key string, item int) map[string][]int {
-	mut m := o
+	mut m := o.clone()
 	if key in m {
 		mut slice := m[key]
 		slice << item
@@ -31,7 +31,7 @@ pub fn str_ints_map_append(o map[string][]int, key string, item int) map[string]
 // str_flts_map_append appends a new item to a map of slice.
 // Note: this function creates a new slice in the map if key is not found.
 pub fn str_flts_map_append(o map[string][]f64, key string, item f64) map[string][]f64 {
-	mut m := o
+	mut m := o.clone()
 	if key in m {
 		mut slice := m[key]
 		slice << item
