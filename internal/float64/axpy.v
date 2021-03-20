87d5c24ced55bed3f01dd0b57d8c77157550a3f8
ulises-jeremias <ulisescf.24@gmail.com>
2021-03-20 05:35:08 -0300

    Added f64 internals

---
+++ internal/float64/axpy.v
@@ -0,0 +1,39 @@
+module float64
+
+// axpy_unitary
+pub fn axpy_unitary(alpha f64, x []f64, mut y []f64) {
+	for i, v in x {
+		y[i] += alpha * v
+	}
+}
+
+// axpy_unitary_to
+pub fn axpy_unitary_to(mut dst []f64, alpha f64, x []f64, y []f64) {
+	for i, v in x {
+		dst[i] = alpha*v + y[i]
+	}
+}
+
+// axpy_inc
+pub fn axpy_inc(alpha f64, x []f64, mut y []f64, n u32, incX u32, incY u32, ix u32, iy u32) {
+        mut ix_ := ix
+        mut iy_ := iy
+	for i := 0; i < int(n); i++ {
+		y[iy_] += alpha * x[ix_]
+		ix_ += incX
+		iy_ += incY
+	}
+}
+
+// axpy_inc_to
+pub fn axpy_inc_to(mut dst []f64, inc_dst u32, idst u32, alpha f64, x []f64, y []f64, n u32, incX u32, incY u32, ix u32, iy u32) {
+        mut ix_ := ix
+        mut iy_ := iy
+        mut idst_ := idst
+	for i := 0; i < int(n); i++ {
+		dst[idst] = alpha*x[ix_] + y[iy_]
+		ix_ += incX
+		iy_ += incY
+		idst_ += inc_dst
+	}
+}
---
+++ internal/float64/dot.v
@@ -0,0 +1,24 @@
+module float64
+
+// dot_unitary
+pub fn dot_unitary(x []f64, y []f64) f64 {
+        mut sum := 0.0
+	for i, v in x {
+		sum += y[i] * v
+	}
+	return sum
+}
+
+// dot_inc
+pub fn dot_inc(x []f64, y []f64, n u32, incX u32, incY u32, ix u32, iy u32) f64 {
+        mut sum := 0.0
+        mut ix_ := ix
+        mut iy_ := iy
+	for i := 0; i < int(n); i++ {
+		sum += y[iy_] * x[ix_]
+		ix_ += incX
+		iy_ += incY
+	}
+	return sum
+}
+
