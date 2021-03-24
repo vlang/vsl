f039c6285b444a88f27bc0cf423b578c44288a26
ulises-jeremias <ulisescf.24@gmail.com>
2021-03-24 03:28:27 -0300

    Updated tests

---
+++ blas/cflags_notd_cblas.v
@@ -0,0 +1,8 @@
+module blas
+
+#flag linux -O2 -I/usr/local/include -I/usr/lib -I@VROOT/blas
+#flag linux -llapacke -L/usr/local/lib -L/usr/lib
+#flag windows -O2
+#flag windows -lopenblas -lgfortran
+#flag darwin -I/usr/local/opt/openblas/include -I@VROOT/blas
+#flag darwin -lopenblas -L/usr/local/opt/openblas/lib
