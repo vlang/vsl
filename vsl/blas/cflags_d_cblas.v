6c0f54a4689b9cf744d3202c8dba7d7bea3eb11e
ulises-jeremias <ulisescf.24@gmail.com>
2021-04-02 21:25:38 -0300

    Updated module

--- vsl/blas/cflags_d_cblas.v
+++ vsl/blas/cflags_d_cblas.v
@@ -1,8 +1,8 @@
 module blas
 
-#flag linux -O2 -I/usr/local/include -I/usr/lib -I@VROOT/vsl/blas
+#flag linux -O2 -I/usr/local/include -I/usr/lib -I@VROOT
 #flag linux -lopenblas -llapacke -L/usr/local/lib -L/usr/lib
 #flag windows -O2
 #flag windows -lopenblas -lgfortran
-#flag darwin -I/usr/local/opt/openblas/include -I@VROOT/vsl/blas
+#flag darwin -I/usr/local/opt/openblas/include -I@VROOT
 #flag darwin -lopenblas -L/usr/local/opt/openblas/lib
