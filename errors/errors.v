d9023f8bc89430bfe53f6c3073c9361b952e161e
ulises-jeremias <ulisescf.24@gmail.com>
2021-07-10 12:31:04 -0300

    Updated errors

--- errors/errors.v
+++ errors/errors.v
@@ -2,24 +2,20 @@ module errors
 
 [inline]
 pub fn vsl_error(reason string, errno ErrorCode) IError {
-	estr := errno.str()
-	return error_with_code('vsl: $estr: $reason', int(errno))
+	return error_with_code('vsl: $errno: $reason', int(errno))
 }
 
 [inline]
 pub fn vsl_panic(reason string, errno ErrorCode) {
-	estr := errno.str()
-	panic('vsl: $estr: $reason')
+	panic('vsl: $errno: $reason')
 }
 
 [inline]
 pub fn vsl_error_message(reason string, errno ErrorCode) string {
-	estr := errno.str()
-	return 'vsl: $estr: $reason'
+	return 'vsl: $errno: $reason'
 }
 
 [inline]
 pub fn vsl_panic_message(reason string, errno ErrorCode) string {
-	estr := errno.str()
-	return 'vsl: $estr: $reason'
+	return 'vsl: $errno: $reason'
 }
