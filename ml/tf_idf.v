8fd8b6ab1e827f0d11e5c1c9779ca0b61bb22d4c
lucasrdrgs <lucasrodriguespimentel@gmail.com>
2021-07-14 18:30:45 -0300

    Tried to optimize it a little bit

--- ml/tf_idf.v
+++ ml/tf_idf.v
@@ -22,30 +22,22 @@ pub fn term_frequencies(ngrams_sentence [][]string) map[string]f64 {
 	}
 	mut tf := map[string]f64
 	joined := ngrams_sentence.map(it.join(ml.ngram_sep))
-	mut used_ngrams := []string{}
+	mut freq_map := map[string]int
 	for ngram in joined {
-		if ngram in used_ngrams { continue }
-		mut ngram_frequency := 0.0
-		for ngram2 in joined {
-			if ngram == ngram2 {
-				ngram_frequency += 1.0
-			}
+		if ngram !in freq_map {
+			freq_map[ngram] = 0
 		}
-		tf[ngram] = ngram_frequency / f64(ngrams_sentence.len)
-		used_ngrams << ngram
+		freq_map[ngram]++
+	}
+	for ngram in joined {
+		tf[ngram] = freq_map[ngram] / f64(ngrams_sentence.len)
 	}
 	return tf
 }
 
-// inverse_document_frequencies will return the IDF of each term.  However,
-// since in VSL NLP tools we deal with ngrams instead of regular "words"
-// (1gram tokens), our document is actually a collection of ngrams and our
-// words/terms are the ngrams themselves. This means that a document (a
-// collection of sentences) is actually a `[][][]string` (array of sentences,
-// which by themselves are arrays of ngrams, which are arrays of string).
-// Keep in mind that, since V does NOT support maps of arrays, ngrams are
-// joined by the constant `ngram_sep`, which can be found in `ml/tokenizer.v`.
-// This is why it returns a `map[string]f64` and not a `map[[]string]f64`.
+// inverse_document_frequencies will return the IDF of each term by calling
+// `term_idf` on each unique ngram. Check `term_idf` for more details about
+// the parameter `document`.
 pub fn inverse_document_frequencies(document [][][]string) map[string]f64 {
 	if document.len == 0 {
 		errors.vsl_panic(
@@ -66,26 +58,53 @@ pub fn inverse_document_frequencies(document [][][]string) map[string]f64 {
 	}
 
 	for ngram in unique_ngrams {
-		mut n_sentences_with_ngram := 0
-		for sentence in document {
-			if ngram in sentence.map(it.join(ml.ngram_sep)) {
-				n_sentences_with_ngram++
-			}
-		}
-		// log base doesnt really matter, as long as it is indeed a logarithm.
-		// I chose 2 because it seems to be the most common log base for computer
-		// science in general.
-		idf[ngram] = math.log2(
-			f64(document.len) / f64(n_sentences_with_ngram)
-		)
+		idf[ngram] = term_idf(ngram.split(ml.ngram_sep), document)
 	}
 	return idf
+}
+
+// term_idf will return the IDF of a single term term.  However, since in
+// VSL NLP tools we deal with ngrams instead of regular "words" (1gram
+// tokens), our document is actually a collection of ngrams and our
+// words/terms are the ngrams themselves. This means that a document (a
+// collection of sentences) is actually a `[][][]string` (array of sentences,
+// which by themselves are arrays of ngrams, which are arrays of string).
+// Keep in mind that, since V does NOT support maps of arrays, ngrams are
+// joined by the constant `ngram_sep`, which can be found in `ml/tokenizer.v`.
+// This is why it returns a `map[string]f64` and not a `map[[]string]f64`.
+pub fn term_idf(term []string, document [][][]string) f64 {
+	if document.len == 0 {
+		errors.vsl_panic(
+			'ml.term_idf expects at least one sentence.',
+			.einval
+		)
+	}
+	if term.len == 0 {
+		errors.vsl_panic(
+			'ml.term_idf expects at least one ngram.',
+			.einval
+		)
+	}
+
+	ngram := term.join(ml.ngram_sep)
+
+	mut n_sentences_with_ngram := 0
+	for sentence in document {
+		if ngram in sentence.map(it.join(ml.ngram_sep)) {
+			n_sentences_with_ngram++
+		}
+	}
+
+	// log base doesnt really matter, as long as it is indeed a logarithm.
+	// I chose 2 because it seems to be the most common log base for computer
+	// science in general.
+	return math.log2(f64(document.len) / f64(n_sentences_with_ngram))
 }
 
 // tf_idf will return the TF * IDF for any given ngram, in a sentence, in a
 // document.
 pub fn tf_idf(ngram []string, sentence [][]string, document [][][]string) f64 {
 	tf := term_frequencies(sentence)[ngram.join(ml.ngram_sep)]
-	idf := inverse_document_frequencies(document)[ngram.join(ml.ngram_sep)]
+	idf := term_idf(ngram, document)
 	return tf * idf
 }
