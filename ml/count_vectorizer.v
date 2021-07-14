29fed7bd43204b37edc9273d22e0feac560d6a82
lucasrdrgs <lucasrodriguespimentel@gmail.com>
2021-07-14 16:41:19 -0300

    Added count_vectorizer, ngrams, remove_stopwords, etc

---
+++ ml/count_vectorizer.v
@@ -0,0 +1,70 @@
+module ml
+
+import vsl.errors
+
+const ngram_sep = 'NGRAMSEP'
+
+// most_frequent_ngrams returns an array with up to `n_features` elements
+// denoting the most frequent ngrams in `ngrams`.
+// Since V does not support map of arrays, the ngrams are joined by
+// "NGRAMSEP". If `n_features` is <= 0, it will be set to ngrams.len.
+pub fn most_frequent_ngrams(ngrams [][]string, n_features int) [][]string {
+	if ngrams.len == 0 {
+		errors.vsl_panic(
+			'ngram_frequency_map expects a non-empty array of ngrams.',
+			.einval
+		)
+	}
+	mut n_features_ := n_features
+	if n_features <= 0 {
+		n_features_ = ngrams.len
+	}
+	else if n_features > ngrams.len {
+		errors.vsl_panic(
+			'n_features cannot be greater than the amount of ngrams.',
+			.einval
+		)
+	}
+	mut freq_map := map[string]int
+	mut joined := ngrams.map(it.join(ngram_sep))
+	// ngrams are joined by `ngram_sep` due to maps
+	// not supporting []string keys yet.
+	for ngram in joined {
+		if ngram !in freq_map {
+			freq_map[ngram] = 0
+		}
+		freq_map[ngram]++
+	}
+	mut result := [][]string{}
+	for _ in 0..n_features_ {
+		keys := freq_map.keys()
+		if keys.len == 0 { break }
+		mut most_frequent := keys[0]
+		for ngram in keys {
+			if freq_map[ngram] > freq_map[most_frequent] {
+				most_frequent = ngram
+			}
+		}
+		result << most_frequent.split(ngram_sep)
+		freq_map.delete(most_frequent)
+	}
+	return result
+}
+
+// count_vectorize will give you an array of occurrences of each
+// ngram from `ngrams` in `most_frequent`.
+// Example: assume `ng := [['hello'], ['hello'], ['hi']]`.
+// `ml.count_vectorize(ng, ml.most_frequent_ngrams(ng, 0))`
+// should return `[2, 1]`. See `most_frequent_ngrams for more`
+// details on how it works.
+pub fn count_vectorize(ngrams [][]string, most_frequent [][]string) []int {
+	mut result := []int{len: most_frequent.len, init: 0}
+	for ngram in ngrams {
+		for i, m_f in most_frequent {
+			if ngram == m_f {
+				result[i]++
+			}
+		}
+	}
+	return result
+}
--- ml/tokenizer.v
+++ ml/tokenizer.v
@@ -6,9 +6,22 @@ const punctuation = (
 	',.[]()[]-=_*;:+><\\"`´~^!?@#$%¨&/|'.split('')
 )
 
-// tokenize_ does the same as tokenize, but does not emit a warning.
-pub fn tokenize_(x string) []string {
-	new_x := x.replace_each(['\n', ' ', '\t', ' ', '\v', ' ', '\r', ' '])
+// remove_punctuation will remove the following characters from the string:
+// `,.[]()[]-=_*;:+><\\"`´~^!?@#$%¨&/|'`
+pub fn remove_punctuation(x string) string {
+	mut new_x := x
+	for punct in (punctuation.join('') + '\'').split('') {
+		new_x = new_x.replace(punct, '')
+	}
+	return new_x
+}
+
+// tokenize will return an array of tokens from the string `x`.
+pub fn tokenize(x string) []string {
+	mut new_x := x
+	for punct in '\n\t\v\r'.split('') {
+		new_x = new_x.replace(punct, '')
+	}
 	mut tokens := []string{}
 
 	mut current_piece := ''
@@ -21,7 +34,9 @@ pub fn tokenize_(x string) []string {
 		}
 
 		if current_char == ' ' {
-			tokens << current_piece
+			if current_piece.len > 0 {
+				tokens << current_piece
+			}
 			current_piece = ''
 			i++
 			continue
@@ -39,17 +54,84 @@ pub fn tokenize_(x string) []string {
 		i++
 	}
 
+	if current_piece.len > 0 {
+		tokens << current_piece
+	}
+
 	return tokens
 }
 
-// tokenize will warn about the lack of complexity of the implementation
-// and will return an array of tokens from the string `x`.
-pub fn tokenize(x string) []string {
-	print(
-		'vsl: Warning: vsl.ml.tokenize is still in development. It is not ' +
-		'very advanced as of now, performing only simple things such as ' +
-		'splitting by spaces and punctuation. To get rid of this message, ' +
-		'run tokenize_(your_string) instead.'
-	)
-	return tokenize_(x)
+// remove_stopwords will remove all tokens included in `stopwords`.
+// If `ignore_case` is true, "THIS" will be considered as "this".
+pub fn remove_stopwords(tokens []string, stopwords []string, ignore_case bool) []string {
+	mut result := []string{}
+	for tok_ in tokens {
+		mut tok := tok_
+		if ignore_case {
+			tok = tok.to_lower()
+		}
+		if tok !in stopwords {
+			result << tok
+		}
+	}
+	return result
+}
+
+// remove_stopwords_en is a wrapper for `remove_stopwords`, passing
+// a default array of some English stopwords.
+pub fn remove_stopwords_en(tokens []string, ignore_case bool) []string {
+	return remove_stopwords(tokens, [
+		'a', 'about', 'above', 'after', 'again', 'against', 'ain',
+		'ain\'t', 'all', 'am', 'an', 'and', 'any', 'are', 'aren',
+		'aren\'t', 'as', 'at', 'be', 'because', 'been', 'before',
+		'being', 'below', 'between', 'both', 'but', 'by', 'can',
+		'couldn', 'couldn\'t', 'd', 'did', 'didn', 'didn\'t', 'do',
+		'does', 'doesn', 'doesn\'t', 'doing', 'don', 'don\'t',
+		'down', 'during', 'each', 'few', 'for', 'from', 'further',
+		'had', 'hadn', 'hadn\'t', 'has', 'hasn', 'hasn\'t', 'have',
+		'haven', 'haven\'t', 'having', 'he', 'her', 'here', 'hers',
+		'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+		'into', 'is', 'isn', 'isn\'t', 'it', 'it\'s', 'its', 'itself',
+		'just', 'll', 'm', 'ma', 'me', 'mightn', 'mightn\'t', 'more',
+		'most', 'mustn', 'mustn\'t', 'my', 'myself', 'needn', 'needn\'t',
+		'now', 'o', 'of', 'off', 'on', 'once', 'only', 'or', 'other',
+		'our', 'ours', 'ourselves', 'out', 'over', 'own', 're', 's',
+		'same', 'shan', 'shan\'t', 'she', 'she\'s', 'should', 'should\'ve',
+		'shouldn', 'shouldn\'t', 'so', 'some', 'such', 't', 'than', 'that',
+		'that\'ll', 'the', 'their', 'theirs', 'them', 'themselves', 'then',
+		'there', 'these', 'they', 'this', 'those', 'through', 'to', 'too',
+		'under', 'until', 'up', 've', 'very', 'was', 'wasn', 'wasn\'t', 'we',
+		'were', 'weren', 'weren\'t', 'what', 'when', 'where', 'which',
+		'while', 'who', 'whom', 'why', 'will', 'with', 'won', 'won\'t',
+		'wouldn', 'wouldn\'t', 'y', 'you', 'you\'d', 'you\'ll', 'you\'re',
+		'you\'ve', 'your', 'yours', 'yourself', 'yourselves', 'could',
+		'he\'d', 'he\'ll', 'he\'s', 'here\'s', 'how\'s', 'i\'d', 'i\'ll',
+		'i\'m', 'i\'ve', 'let\'s', 'ought', 'she\'d', 'she\'ll', 'that\'s',
+		'there\'s', 'they\'d', 'they\'ll', 'they\'re', 'they\'ve', 'we\'d',
+		'we\'ll', 'we\'re', 'we\'ve', 'what\'s', 'when\'s', 'where\'s',
+		'who\'s', 'why\'s', 'would'
+	], ignore_case)
+}
+
+// ngrams will return an array of grams containing `n` elements
+// from `tokens`. Example: `ngrams('the apple is red'.split(' '), 3)`
+// will return [['the', 'apple', 'is'], ['apple', 'is', 'red']]
+pub fn ngrams(tokens []string, n int) [][]string {
+	mut grams := [][]string{}
+	if tokens.len == 0 {
+		return grams
+	}
+	if n > tokens.len {
+		return [tokens]
+	}
+	if n <= 0 {
+		errors.vsl_panic(
+			'ml.ngrams expects n to be in the inclusive range [0, tokens.len].',
+			.einval
+		)
+	}
+	for i in 0..(tokens.len - n + 1) {
+		grams << tokens[i..(i + n)]
+	}
+	return grams
 }
