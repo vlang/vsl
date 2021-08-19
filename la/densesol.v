00447e681f319ce1d8f5cd142d0327cae9719124
ulises-jeremias <ulisescf.24@gmail.com>
2021-08-18 23:54:55 -0300

    Updated fmt

--- la/densesol.v
+++ la/densesol.v
@@ -7,10 +7,10 @@ import vsl.blas
 //   Given:  a ⋅ x = b    find x   such that   x = a⁻¹ ⋅ b
 //
 pub fn den_solve(mut x []f64, a Matrix, b []f64, preserve_a bool) {
-        mut a_ := a.transpose()
+	mut a_ := a.transpose()
 	if preserve_a {
 		a_ = new_matrix(a.m, a.n)
-                a_.data = a.transpose().data.clone()
+		a_.data = a.transpose().data.clone()
 	}
 	for i in 0 .. x.len {
 		x[i] = b[i]
