639f381dc110fd0c5cdffe761af15b08f4192954
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2021-10-11 17:20:37 -0300

    Update vector_test.v

--- la/vector_test.v
+++ la/vector_test.v
@@ -2,10 +2,7 @@ module la
 
 import math
 import math.util
-
-fn feq(a f64, n f64) bool {
-	return util.fabs_64(a - n) < math.max(n / 1000, 0.001) // 0.1% error, or min diff
-}
+import vsl.float.float64
 
 fn test_vector_apply() {
 	mut a := []f64{len: 5}
@@ -39,30 +36,30 @@ fn test_vector_unit() {
 fn test_vector_accum() {
 	a := [1.0, 2, -3]
 	s := 0.0
-	assert feq(vector_accum(a), s)
+	assert float64.close(vector_accum(a), s)
 	// assert vector_accum(a) == s
 }
 
 fn test_vector_norm() {
 	a := [3.0, 4]
 	n := 5.0
-	assert feq(vector_norm(a), n)
+	assert float64.close(vector_norm(a), n)
 }
 
 fn test_vector_rms() {
 	octave_val := 3.5355 // Calculated in GNU octave
-	assert feq(vector_rms([3.0, 4]), octave_val)
+	assert float64.close(vector_rms([3.0, 4]), octave_val)
 }
 
 fn test_vector_norm_diff() {
 	a := [4.0, 7]
 	b := [1.0, 3]
 	n := 5.0
-	assert feq(vector_norm_diff(a, b), n)
+	assert float64.close(vector_norm_diff(a, b), n)
 }
 
 fn test_vector_largest() {
 	a := [3.0, 4]
 	l := 2.0
-	assert feq(vector_largest(a, 2), l)
+	assert float64.close(vector_largest(a, 2), l)
 }
