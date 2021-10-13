ce71c23f97a52342b9e5d2e554c4487a4708c7a5
ulises-jeremias <ulisescf.24@gmail.com>
2021-10-13 01:11:44 -0300

    Updated flags

--- blas/cflags_d_cblas.v
+++ blas/cflags_d_cblas.v
@@ -4,5 +4,5 @@ module blas
 #flag linux -lopenblas -llapacke -L/usr/local/lib -L/usr/lib
 #flag windows -O2
 #flag windows -lopenblas -lgfortran
-#flag darwin -I/usr/local/opt/openblas/include -I@VMODROOT
-#flag darwin -lopenblas -L/usr/local/opt/openblas/lib
+#flag darwin -I/usr/local/opt/openblas/include -I/usr/local/opt/lapack/include -I@VMODROOT
+#flag darwin -lopenblas -llapack -L/usr/local/opt/openblas/lib -L/usr/local/opt/lapack/lib
