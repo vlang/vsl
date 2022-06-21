a1729d081dbf1f199f04d7e4c9621ac633a1701a
Delyan Angelov <delian66@gmail.com>
2022-06-21 20:55:10 +0300

    fix compilation with latest V (after c160ba2)

--- la/densesol.v
+++ la/densesol.v
@@ -1,13 +1,13 @@
 module la
 
-import vsl.blas
+import blas
 
 // den_solve solves dense linear system using LAPACK (OpenBLaS)
 //
 //   Given:  a ⋅ x = b    find x   such that   x = a⁻¹ ⋅ b
 //
 pub fn den_solve(mut x []f64, a &Matrix<f64>, b []f64, preserve_a bool) {
-	mut a_ := a
+	mut a_ := unsafe { a }
 	if preserve_a {
 		a_ = new_matrix<f64>(a.m, a.n)
 		a_.data = a.data.clone()
