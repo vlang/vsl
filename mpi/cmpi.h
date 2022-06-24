6b43da43024f2323d2ce03ac48663cf3ce52c89d
ulises-jeremias <ulisescf.24@gmail.com>
2022-06-24 19:18:38 -0300

    Removed some declarations

--- mpi/cmpi.h
+++ mpi/cmpi.h
@@ -3,15 +3,4 @@
 
 #include "mpi.h"
 
-MPI_Comm World = MPI_COMM_WORLD;
-MPI_Op OpSum = MPI_SUM;
-MPI_Op OpMin = MPI_MIN;
-MPI_Op OpMax = MPI_MAX;
-MPI_Datatype TyLong = MPI_LONG;
-MPI_Datatype TyDouble = MPI_DOUBLE;
-MPI_Datatype TyComplex = MPI_DOUBLE_COMPLEX;
-MPI_Status *StIgnore = MPI_STATUS_IGNORE;
-
-#define DOUBLE_COMPLEX double complex
-
 #endif
