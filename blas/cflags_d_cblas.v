9a4bfda7550112b0f8f01b4d4f886137734e94c0
ulises-jeremias <ulisescf.24@gmail.com>
2022-07-03 20:21:51 -0300

    Fix paths

--- blas/cflags_d_cblas.v
+++ blas/cflags_d_cblas.v
@@ -4,5 +4,5 @@ module blas
 #flag linux -lopenblas -llapacke -L/usr/local/lib -L/usr/lib
 #flag windows -O2
 #flag windows -lopenblas -lgfortran
-#flag darwin -I/usr/local/opt/openblas/include -I@VMODROOT
-#flag darwin -lopenblas -L/usr/local/opt/openblas/lib
+#flag darwin -I/usr/local/opt/openblas/include -I/usr/local/include -I/usr/lib -I@VMODROOT
+#flag darwin -lopenblas -L/usr/local/opt/openblas/lib -L/usr/local/lib -L/usr/lib
