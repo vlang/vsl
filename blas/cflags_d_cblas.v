eda352870671f8f8fc98a0227c895b355093c3ab
ulises-jeremias <ulisescf.24@gmail.com>
2022-07-03 20:35:59 -0300

    ?

--- blas/cflags_d_cblas.v
+++ blas/cflags_d_cblas.v
@@ -5,4 +5,4 @@ module blas
 #flag windows -O2
 #flag windows -lopenblas -lgfortran
 #flag darwin -I/usr/local/opt/openblas/include -I/usr/local/include -I/usr/lib -I/opt/homebrew/opt/openblas/include -I@VMODROOT
-#flag darwin -lopenblas -L/usr/local/opt/openblas/lib -L/usr/local/lib -L/usr/lib -L/opt/homebrew/opt/openblas/lib
+#flag darwin -lopenblas -llapacke -L/usr/local/opt/openblas/lib -L/usr/local/lib -L/usr/lib -L/opt/homebrew/opt/openblas/lib
