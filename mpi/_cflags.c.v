e224077885db654be1464b8d6860803046cb28d3
j <j-bm@bitminer.ca>
2022-08-22 19:57:13 +0000

    fix communicators, add typed functions

--- mpi/_cflags.c.v
+++ mpi/_cflags.c.v
@@ -6,5 +6,7 @@ module mpi
 #flag darwin -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.0.1_2/lib -lmpi
 #flag freebsd -I/usr/local/include -I@VMODROOT
 #flag freebsd -L/usr/local/lib -lmpi
+#flag openbsd -I/usr/local/include -I@VMODROOT
+#flag openbsd -L/usr/local/lib -lmpi
 
 #include <cmpi.h>
