33f6623958a25164a9ffacbe34edf9871b071aef
Delyan Angelov <delian66@gmail.com>
2022-10-16 18:10:17 +0300

    run `v fmt -w .`, cleanup WTF

--- iter/prod.v
+++ iter/prod.v
@@ -17,7 +17,7 @@ pub fn new_product_iterator(data [][]f64) ProductIterator {
 		repeat_lengths: calc_repeat_lengths(data.map(it.len)).map(u64(it))
 		indices_to_grab: []int{len: data.len, init: -1}
 		data: data
-		size: u64(arrays.fold( data.map(it.len), 1, int_prod))
+		size: u64(arrays.fold(data.map(it.len), 1, int_prod))
 	}
 }
 
