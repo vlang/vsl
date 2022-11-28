1b6c6b2ccc37dbde60d072cdf563a7ed58604952
ulises-jeremias <ulisescf.24@gmail.com>
2022-11-28 01:52:59 -0300

    Added ML example using plot

--- examples/ml_kmeans_plot02/main.v
+++ examples/ml_kmeans_plot02/main.v
@@ -2,6 +2,7 @@ module main
 
 import vsl.ml
 import vsl.plot
+import dataset
 
 fn color_from_class(i int) string {
 	return match i {
@@ -20,23 +21,15 @@ fn color_from_class(i int) string {
 }
 
 // data
-mut data := ml.data_from_raw_x([
-	[0.1, 0.7],
-	[0.3, 0.7],
-	[0.1, 0.9],
-	[0.3, 0.9],
-	[0.7, 0.1],
-	[0.9, 0.1],
-	[0.7, 0.3],
-	[0.9, 0.3],
-])?
+mut data := ml.data_from_raw_x(dataset.raw_dataset.map([it[0], it[1]]))?
 
 // model
-nb_classes := 2
+nb_classes := 3
 mut model := ml.new_kmeans(mut data, nb_classes, 'kmeans')
 model.set_centroids([
-	[0.4, 0.6] /* class 0 */,
-	[0.6, 0.4] /* class 1 */,
+	[3.0, 3] /* class 0 */,
+	[6.0, 2] /* class 1 */,
+	[8.0, 5] /* class 2 */,
 ])
 
 // initial classes
@@ -47,22 +40,6 @@ model.compute_centroids()
 
 // train
 model.train(epochs: 6)
-
-// test
-expected_classes := [
-	0,
-	0,
-	0,
-	0,
-	1,
-	1,
-	1,
-	1,
-]
-for i, c in model.classes {
-	assert c == expected_classes[i]
-	println('class ${i}: ${c}')
-}
 
 mut plt := plot.new_plot()
 plt.set_layout(
@@ -89,7 +66,7 @@ y := data.x.get_col(1)
 for i in 0 .. nb_classes {
 	mut x_for_class := []f64{}
 	mut y_for_class := []f64{}
-	for j in 0 .. data.y.len {
+	for j in 0 .. data.nb_samples {
 		if model.classes[j] == i {
 			x_for_class << x[j]
 			y_for_class << y[j]
@@ -103,8 +80,8 @@ for i in 0 .. nb_classes {
 		y: y_for_class
 		mode: 'markers'
 		marker: plot.Marker{
-			size: []f64{len: data.y.len, init: 12.0}
-			color: []string{len: data.y.len, init: color_from_class(i)}
+			size: []f64{len: data.nb_samples, init: 12.0}
+			color: []string{len: data.nb_samples, init: color_from_class(i)}
 		}
 	)
 }
