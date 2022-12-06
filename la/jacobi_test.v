80887e690d39250fecb08a9d57086634d56738f6
ulises-jeremias <ulisescf.24@gmail.com>
2022-12-05 21:38:01 -0300

    Added jacobi solver tests

---
+++ la/jacobi_test.v
@@ -0,0 +1,27 @@
+module la
+
+import vsl.float.float64
+
+fn test_jacobi01() {
+	mut a := matrix_deep2([
+		[2.0, 0, 0],
+		[0.0, 2, 0],
+		[0.0, 0, 2],
+	])
+
+	mut q := new_matrix[f64](3, 3)
+	mut v := []f64{len: 3}
+
+	mut expected_q := matrix_deep2([
+		[1.0, 0, 0],
+		[0.0, 1, 0],
+		[0.0, 0, 1],
+	])
+
+	mut expected_v := [2.0, 2.0, 2.0]
+
+	jacobi(mut q, mut v, mut a)?
+
+	assert float64.arrays_tolerance(q.data, expected_q.data, 1e-17)
+	assert float64.arrays_tolerance(v, expected_v, 1e-17)
+}
