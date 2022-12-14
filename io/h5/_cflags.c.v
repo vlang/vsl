a484d74abdb8d25b50f9c0870741ead6675f8bcd
ulises-jeremias <ulisescf.24@gmail.com>
2022-12-14 20:34:49 -0300

    Fix

--- io/h5/_cflags.c.v
+++ io/h5/_cflags.c.v
@@ -1,13 +1,14 @@
 module h5
 
-#flag linux -I/usr/local/include
+#flag linux -I/usr/local/include -I@VMODROOT
 #flag linux -L/usr/local/lib -pthread -lhdf5 -lhdf5_hl
-#flag darwin
+#flag darwin -I@VMODROOT
 #flag darwin -lhdf5 -lhdf5_hl
-#flag freebsd -I/usr/local/include
+#flag freebsd -I/usr/local/include -I@VMODROOT
 #flag freebsd -L/usr/local/lib
-#flag openbsd -I/usr/local/include
+#flag openbsd -I/usr/local/include -I@VMODROOT
 #flag openbsd -L/usr/local/lib
+
 #flag -lhdf5
 #flag -lhdf5_hl
 
