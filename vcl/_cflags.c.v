8121003104ab6bafe9650b188ab919f55e36dc2e
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2023-01-08 17:43:28 +0100

    Update _cflags.c.v

--- vcl/_cflags.c.v
+++ vcl/_cflags.c.v
@@ -1,9 +1,14 @@
 module vcl
 
+$if dlnc {
 #flag linux -I@VMODROOT
-#flag linux -lOpenCL
+#flag linux -lOpenCL               
 #flag windows -lOpenCL
 #flag darwin -I@VMODROOT
 #flag darwin -framework OpenCL
 
 #include <vcl.h>
+}else{
+
+}
+        
