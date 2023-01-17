0afb25a88a1e644626248cac9e2b89482d264513
ulises-jeremias <ulisescf.24@gmail.com>
2023-01-17 16:34:57 -0300

    Updated paths

--- io/h5/_cflags.c.v
+++ io/h5/_cflags.c.v
@@ -1,13 +1,14 @@
 module h5
 
-#flag linux -I/usr/include/hdf5/serial/ -I/usr/local/include -I@VMODROOT
+#flag linux -I/usr/include/hdf5/serial/ -I/usr/local/include
 #flag linux -L/usr/lib -L/usr/lib/x86_64-linux-gnu/hdf5/serial/ -L/usr/local/lib -pthread -lhdf5 -lhdf5_hl
-#flag darwin -I/usr/local/include -I@VMODROOT
+#flag darwin -I/usr/local/include
 #flag darwin -lhdf5 -lhdf5_hl
-#flag freebsd -I/usr/local/include -I@VMODROOT
+#flag freebsd -I/usr/local/include
 #flag freebsd -L/usr/local/lib
-#flag openbsd -I/usr/local/include -I@VMODROOT
+#flag openbsd -I/usr/local/include
 #flag openbsd -L/usr/local/lib
+#flag -I@VMODROOT
 
 #flag -lhdf5
 #flag -lhdf5_hl
--- vlas/cflags_d_cblas.v
+++ vlas/cflags_d_cblas.v
@@ -1,8 +1,13 @@
 module vlas
 
-#flag linux -O2 -I/usr/local/include -I/usr/lib -I@VMODROOT
+#flag linux -O2 -I/usr/local/include -I/usr/lib
 #flag linux -lopenblas -llapacke -L/usr/local/lib -L/usr/lib
 #flag windows -O2
 #flag windows -lopenblas -lgfortran
-#flag darwin -I/usr/local/opt/openblas/include -I@VMODROOT
+#flag darwin -I/usr/local/opt/openblas/include
 #flag darwin -lopenblas -L/usr/local/opt/openblas/lib
+#flag freebsd -I/usr/local/include
+#flag freebsd -L/usr/local/lib
+#flag openbsd -I/usr/local/include
+#flag openbsd -L/usr/local/lib
+#flag -I@VMODROOT
