7d0671792cff546469ed66331ccc29ff0d3b09a2
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2023-01-17 19:59:16 +0100

    simplify flags

--- vcl/_cflags_d_dlopencl.v
+++ vcl/_cflags_d_dlopencl.v
@@ -1,9 +1,5 @@
 module vcl
 
-#flag linux -I@VMODROOT
-#flag termux -I@VMODROOT
-//#flag android -I@VMODROOT TODO try it
-#flag windows -I@VMODROOT
-#flag darwin -I@VMODROOT
+#flag -I@VMODROOT
 
 #include <vcl.h>
