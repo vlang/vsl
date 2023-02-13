f39e0694f14c1eac7fb304a0a0c9540dee1a07a3
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2023-02-13 11:21:23 +0100

    Add files via upload

--- vcl/image.c.v
+++ vcl/image.c.v
@@ -40,14 +40,14 @@ pub fn (mut img Image) release() ? {
 	return img.buf.release()
 }
 
-// image allocates an image buffer
-pub fn (d &Device) image2d(@type ImageChannelOrder, bounds Rect) ?&Image {
+// image_2d allocates an image buffer
+pub fn (d &Device) image_2d(@type ImageChannelOrder, bounds Rect) ?&Image {
 	println(@STRUCT + '.' + @FN + ' is not stable yet. Issues are expected.')
 	return d.create_image2d(@type, bounds, unsafe { nil })
 }
 
-// from_image creates new Image and copies data from Image
-pub fn (d &Device) from_image2d(img ImageData) ?&Image {
+// from_image_2d creates new Image and copies data from Image
+pub fn (d &Device) from_image_2d(img ImageData) ?&Image {
 	println(@STRUCT + '.' + @FN + ' is not stable yet. Issues are expected.')
 	data := img.data
 	mut image_type := ImageChannelOrder.intensity
@@ -60,19 +60,27 @@ pub fn (d &Device) from_image2d(img ImageData) ?&Image {
 	return d.create_image2d(image_type, bounds, data)
 }
 
-// create_image2d creates a new image
-fn (d &Device) create_image2d(image_type ImageChannelOrder, bounds Rect, data voidptr) ?&Image {
+// create_image_2d creates a new image
+fn (d &Device) create_image_2d(image_type ImageChannelOrder, bounds Rect, data voidptr) ?&Image {
 	mut row_pitch := int(bounds.width)
 	mut size := int(bounds.width * bounds.height)
 	if image_type == ImageChannelOrder.rgba {
 		size *= 4
 		row_pitch *= 4
 	}
-	format := C.create_image_format(usize(image_type), usize(ImageChannelDataType.unorm_int8))
+	format := C.create_image_format(
+		usize(image_type),
+		usize(ImageChannelDataType.unorm_int8))
 	desc := C.create_image_desc(C.CL_MEM_OBJECT_IMAGE2D, usize(bounds.width), usize(bounds.height),
 		0, 0, usize(row_pitch), 0, 0, 0, unsafe { nil })
 
-	mut flags := mem_read_write | mem_copy_host_ptr
+
+	mut flags := mem_read_write
+
+	if !isnil(data) {
+		flags = mem_read_write | mem_copy_host_ptr
+	}
+
 	export_data := if !isnil(data) {
 		data // unsafe {[]u8(data).clone()}
 	} else {
@@ -109,7 +117,7 @@ fn (d &Device) create_image2d(image_type ImageChannelOrder, bounds Rect, data vo
 	return img
 }
 
-pub fn (image &Image) data() ?&Image {
+pub fn (image &Image) data_2d() ?&Image {
 	origin := [3]usize{init: 0}
 	region0 := [usize(image.bounds.width), usize(image.bounds.height), 1]
 	region := [3]usize{init: region0[it]}
@@ -145,4 +153,68 @@ fn (image &Image) write_queue() ?int {
 		return vcl_error(ret)
 	}
 	return ret
+}
+
+// image_general allocates an image buffer TODO not accomplish - broken
+fn (d &Device) image_general(@type ImageChannelOrder, bounds Rect) ?&Image {
+	println(@STRUCT + '.' + @FN + ' is not stable yet. Issues are expected.')
+	return d.create_image(@type, bounds, 0, unsafe { nil })
+}
+
+// from_image_general creates new Image and copies data from Image TODO not accomplish - broken
+fn (d &Device) from_image_general(img ImageData) ?&Image {
+	println(@STRUCT + '.' + @FN + ' is not stable yet. Issues are expected.')
+	data := img.data
+	mut row_pitch := 0
+	mut image_type := ImageChannelOrder.intensity
+
+	if img.nr_channels in [3, 4] {
+		image_type = ImageChannelOrder.rgba
+	}
+
+	bounds := Rect{0, 0, img.width, img.height}
+	return d.create_image(image_type, bounds, row_pitch, data)
+}
+
+// create_image_general creates a new image TODO not accomplish - broken
+fn (d &Device) create_image_general(image_type ImageChannelOrder, bounds Rect, row_pitch int, data voidptr) ?&Image {
+	format := C.create_image_format(usize(image_type), usize(ImageChannelDataType.unorm_int8))
+	desc := C.create_image_desc(C.CL_MEM_OBJECT_IMAGE2D, usize(bounds.width), usize(bounds.height),
+		0, 0, usize(row_pitch), 0, 0, 0, unsafe { nil })
+
+	mut flags := mem_read_write
+
+	if !isnil(data) {
+		flags = mem_read_write | mem_copy_host_ptr
+	}
+
+	mut ret := 0
+
+	memobj := cl_create_image(d.ctx, flags, format, desc, data, &ret)
+	if ret != success {
+		return vcl_error(ret)
+	}
+
+	if isnil(memobj) {
+		return err_unknown
+	}
+
+	mut size := int(bounds.width * bounds.height)
+	if image_type == ImageChannelOrder.rgba {
+		size *= 4
+	}
+
+	buf := &Buffer{
+		memobj: memobj
+		size: size
+		device: d
+	}
+
+	return &Image{
+		buf: buf
+		bounds: bounds
+		@type: image_type
+		format: format
+		desc: desc
+	}
 }
