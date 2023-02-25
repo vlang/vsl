534343e93e011988d6055b528abb145926ad7c7e
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2023-02-25 02:16:52 +0100

    optimalization prime (#155)

--- prime/prime.v
+++ prime/prime.v
@@ -1,21 +1,20 @@
 module prime
 
 import math
-
 // is_prime returns if an int is prime (deterministically)
+
 pub fn is_prime(p int) bool {
-	if p < 2 {
-		return false
+	if p < 2 || p % 2 == 0 {
+		return p == 2
 	}
-	mut i := 2
-	max := math.floor(f32(p) / 2)
+	mut i := 3
+	max := math.floor(math.sqrt(p))
 	for i <= max {
 		rem := p % i
-		if rem != 0 {
-			i++
-		} else {
+		if rem == 0 {
 			return false
 		}
+		i += 2
 	}
 	return true
 }
@@ -25,20 +24,22 @@ pub fn prime_sieve(range int) ?[]int {
 	if range <= 1 {
 		return error('Range must be greater than 1')
 	}
-	mut number_list := []bool{len: range, init: true}
+	mut number_list := []bool{len: range, init: (it % 2 != 0)}
 	number_list[0] = false
 	number_list[1] = false
-	mut primes := []int{cap: range / 2}
+	number_list[2] = true
+	mut primes := []int{cap: int(range / 6)}
 
-	for i in 2 .. int(math.sqrt(range)) {
+	for i := 3; i < int(math.sqrt(range)); i += 2 {
 		if number_list[i] {
 			for j := i * i; j < range; j += i {
 				number_list[j] = false
 			}
 		}
 	}
-	for i, n in number_list {
-		if n {
+	primes << 2
+	for i := 3; i < range; i += 2 {
+		if number_list[i] {
 			primes << i
 		}
 	}
