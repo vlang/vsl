d27d9327df53b36535750fcc3d32b37bcd488f70
ulises-jeremias <ulisescf.24@gmail.com>
2023-03-19 17:18:55 -0300

    Added MPI c flags

--- mpi/_cflags.c.v
+++ mpi/_cflags.c.v
@@ -4,6 +4,10 @@ module mpi
 #flag linux -pthread -L/usr/lib/x86_64-linux-gnu/openmpi/lib -lmpi
 #flag darwin -I/usr/local/Cellar/open-mpi/4.0.1_2/include -I/usr/local/include
 #flag darwin -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.0.1_2/lib -L/usr/local/lib -lmpi
+#flag freebsd -I/usr/local/Cellar/open-mpi/4.0.1_2/include -I/usr/local/include
+#flag freebsd -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.0.1_2/lib -L/usr/local/lib -lmpi
+#flag openbsd -I/usr/local/Cellar/open-mpi/4.0.1_2/include -I/usr/local/include
+#flag openbsd -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.0.1_2/lib -L/usr/local/lib -lmpi
 #flag -I@VMODROOT
 
 #include <cmpi.h>
