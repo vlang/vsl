48bfee788d798e5de1962d111c34ef7d6449d1fa
ulises-jeremias <ulisescf.24@gmail.com>
2023-05-01 13:57:06 -0300

    Updated root solver

--- roots/bisection.v
+++ roots/bisection.v
@@ -7,33 +7,43 @@ import math
 pub struct Bisection {
 	f func.Fn [required]
 pub mut:
-	xmin    f64
-	xmax    f64
-	epsrel  f64
-	epsabs  f64
-	n_max   int
-	n_calls int
-	n_iter  int
+	xmin      f64 // lower bound
+	xmax      f64 // upper bound
+	epsrel    f64 // relative error tolerance
+	epsabs    f64 // absolute error tolerance
+	n_max     int // maximum number of iterations
+	n_f_calls int // number of function calls
+	n_iter    int // number of iterations
+}
+
+// BisectionParams contains the parameters for the bisection method
+[params]
+pub struct BisectionParams {
+	xmin   f64
+	xmax   f64
+	epsrel f64 = 1e-6
+	epsabs f64 = 1e-6
+	n_max  int = 100
 }
 
 // new_bisection creates a new Bisection object
-pub fn new_bisection(f func.Fn) &Bisection {
+pub fn new_bisection(f func.Fn, params BisectionParams) &Bisection {
 	return &Bisection{
 		f: f
-		xmin: 0.0
-		xmax: 0.0
-		epsrel: 1e-6
-		epsabs: 1e-6
-		n_max: 100
+		xmin: params.xmin
+		xmax: params.xmax
+		epsrel: params.epsrel
+		epsabs: params.epsabs
+		n_max: params.n_max
 	}
 }
 
 pub struct BisectionIteration {
 pub mut:
-	x       f64
-	fx      f64
-	n_calls int
-	n_iter  int
+	x         f64
+	fx        f64
+	n_f_calls int
+	n_iter    int
 }
 
 // next returns the next iteration of the bisection method.
@@ -46,12 +56,12 @@ pub fn (mut solver Bisection) next() ?&BisectionIteration {
 	solver.n_iter += 1
 	xmid := (solver.xmin + solver.xmax) / 2.0
 	fxmid := solver.f.safe_eval(xmid) or { return none }
-	solver.n_calls += 1
+	solver.n_f_calls += 1
 	if math.abs(fxmid) < solver.epsabs || math.abs(fxmid) < solver.epsrel * math.abs(fxmid) {
 		return &BisectionIteration{
 			x: xmid
 			fx: fxmid
-			n_calls: solver.n_calls
+			n_f_calls: solver.n_f_calls
 			n_iter: solver.n_iter
 		}
 	}
@@ -65,7 +75,7 @@ pub fn (mut solver Bisection) next() ?&BisectionIteration {
 	return &BisectionIteration{
 		x: xmid
 		fx: fxmid
-		n_calls: solver.n_calls
+		n_f_calls: solver.n_f_calls
 		n_iter: solver.n_iter
 	}
 }
@@ -79,7 +89,7 @@ pub fn (mut solver Bisection) solve() !&BisectionIteration {
 		iter := solver.next() or { break }
 		result.x = iter.x
 		result.fx = iter.fx
-		result.n_calls = iter.n_calls
+		result.n_f_calls = iter.n_f_calls
 		result.n_iter = iter.n_iter
 	}
 	return result
--- roots/roots_test.v
+++ roots/roots_test.v
@@ -1,6 +1,7 @@
 module roots
 
 import vsl.func
+import vsl.float.float64
 import math
 
 const (
@@ -18,34 +19,21 @@ fn fdf_cos(x f64, _ []f64) (f64, f64) {
 }
 
 fn test_root_bisection() {
-	x1 := 0.0
-	x2 := f64(3)
 	f := func.new_func(f: f_cos)
-	mut solver := new_bisection(f)
-	solver.xmin = x1
-	solver.xmax = x2
-	solver.epsrel = roots.epsrel
-	solver.epsabs = roots.epsabs
-	solver.n_max = roots.n_max
+	mut solver := new_bisection(f,
+		xmin: 0.0
+		xmax: 3.0
+		epsrel: roots.epsrel
+		epsabs: roots.epsabs
+		n_max: roots.n_max
+	)
 	result := solver.solve()!
-	assert compare(result.x, math.pi / 2.00)
+	assert float64.soclose(result.x, math.pi / 2.00, roots.epsabs)
 }
 
 fn test_root_newton() {
 	x0 := f64(0.5)
 	f := func.new_func_fdf(fdf: fdf_cos)
 	result := newton(f, x0, roots.epsrel, roots.epsabs, roots.n_max)!
-	assert compare(result, math.pi / 2.00)
-}
-
-// Helper method for comparing floats
-fn compare(x f64, y f64) bool {
-	tolerance := roots.epsabs
-	// Special case for zeroes
-	if x < tolerance && x > (-1.0 * tolerance) && y < tolerance && y > (-1.0 * tolerance) {
-		return true
-	}
-	diff := math.abs(x - y)
-	mean := math.abs(x + y) / 2.0
-	return if math.is_nan(diff / mean) { true } else { ((diff / mean) < tolerance) }
+	assert float64.soclose(result, math.pi / 2.00, roots.epsabs)
 }
