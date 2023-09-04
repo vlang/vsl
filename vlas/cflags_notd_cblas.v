b7051762583b3d125d2bd4052c4f4d35247f45d5
ulises-jeremias <ulisescf.24@gmail.com>
2023-09-04 03:24:56 -0300

    ?

--- vlas/cflags_notd_cblas.v
+++ vlas/cflags_notd_cblas.v
@@ -5,5 +5,5 @@ module vlas
 #flag windows -O2
 #flag windows -lopenblas -lgfortran
 #flag darwin -I/usr/local/opt/openblas/include -I/usr/local/opt/lapack/include
-#flag darwin -llapacke -lopenblas -L/usr/local/opt/openblas/lib -L/usr/local/opt/lapack/lib
+#flag darwin -llapacke -llapack -lopenblas -L/usr/local/opt/openblas/lib -L/usr/local/opt/lapack/lib
 #flag -I@VMODROOT
