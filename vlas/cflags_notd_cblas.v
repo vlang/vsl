0f724c74d705bb42f64caa19ed4388b3e320cadb
ulises-jeremias <ulisescf.24@gmail.com>
2023-09-04 02:11:13 -0300

    Fix?

--- vlas/cflags_notd_cblas.v
+++ vlas/cflags_notd_cblas.v
@@ -4,6 +4,6 @@ module vlas
 #flag linux -llapacke -L/usr/local/lib -L/usr/lib
 #flag windows -O2
 #flag windows -lopenblas -lgfortran
-#flag darwin -I/usr/local/opt/openblas/include -I/usr/local/include
-#flag darwin -lopenblas -L/usr/local/opt/openblas/lib -L/usr/local/lib
+#flag darwin -I/usr/local/opt/openblas/include -I/usr/local/include -I/usr/lib
+#flag darwin -lopenblas -llapacke -L/usr/local/opt/openblas/lib -L/usr/local/lib -L/usr/lib
 #flag -I@VMODROOT
