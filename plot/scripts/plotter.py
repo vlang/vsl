d658535792f8d513c75c09c649df220d5f860607
ulises-jeremias <ulisescf.24@gmail.com>
2023-10-09 00:10:13 -0300

    Updated plotter

--- plot/scripts/plotter.py
+++ plot/scripts/plotter.py
@@ -5,107 +5,101 @@ import plotly.graph_objects as go
 import numpy as np
 
 
-# Remove empty properties as to let Plotly
-# use default ones. If empty_subkey is provided,
-# it will only remove empty_subkey from d[key]
-# (if present and empty). If it is not provided,
-# then it removes key from d if key is in d and
-# d[key] is empty.
 def remove_key(d, key, empty_subkey=None):
-    r = d
-    if key in r:
+    """
+    Remove specified key from dictionary 'd'.
+    If 'empty_subkey' is provided, it removes 'empty_subkey'
+    from 'd[key]' if present and empty. If 'empty_subkey'
+    is not provided, it removes 'key' from 'd' if 'key' is
+    in 'd' and 'd[key]' is empty.
+    """
+    result = d
+    if key in result:
         if empty_subkey is not None:
-            if empty_subkey in r[key]:
-                if not len(r[key][empty_subkey]):
-                    r[key].pop(empty_subkey)
+            if empty_subkey in result[key]:
+                if not len(result[key][empty_subkey]):
+                    result[key].pop(empty_subkey)
         else:
-            if not len(r[key]):
-                r.pop(key)
-    return r
+            if not len(result[key]):
+                result.pop(key)
+    return result
+
+
+def remove_empty_keys(d):
+    """
+    Recursively remove empty keys (both keys with empty values
+    and empty lists/dictionaries).
+    """
+    result = d
+    for k in list(result.keys()):
+        if isinstance(result[k], dict):
+            result[k] = remove_empty_keys(result[k])
+            if not len(result[k]):
+                result.pop(k)
+        elif isinstance(result[k], list):
+            if not len(result[k]):
+                result.pop(k)
+    return result
 
 
 def is_valid_file(parser, arg):
+    """
+    Check if the provided file path exists.
+    """
     if not os.path.exists(arg):
         parser.error("The file %s does not exist!" % arg)
     else:
         return open(arg, "r")  # return an open file handle
 
 
-parser = argparse.ArgumentParser(description="Run training")
-parser.add_argument(
-    "--data",
-    dest="data",
-    required=True,
-    help="input file with data",
-    metavar="FILE",
-    type=lambda x: is_valid_file(parser, x),
-)
-parser.add_argument(
-    "--layout",
-    dest="layout",
-    required=True,
-    help="input file with layout",
-    metavar="FILE",
-    type=lambda x: is_valid_file(parser, x),
-)
-
-args = parser.parse_args()
-
-# Read data json file.
-data = json.load(args.data)
-
-# Read layout json file.
-layout = json.load(args.layout)
-# If there is no range specified, leave it as None
-# and let Plotly handle it.
-for ax in ['x', 'y']:
-    if f'{ax}axis' in layout:
-        if 'range' in layout[f'{ax}axis']:
-            if len(layout[f'{ax}axis']['range']) != 2:
-                layout[f'{ax}axis']['range'] = None
-
-# Map vsl.plot.TraceType enum to Plotly objects.
-type_map = {
-    'scatter': go.Scatter,
-    'pie': go.Pie,
-    'heatmap': go.Heatmap,
-    'surface': go.Surface,
-    'scatter3d': go.Scatter3d,
-    'bar': go.Bar,
-    'histogram': go.Histogram
-}
+def load_json_file(file_handle):
+    """
+    Load and return JSON data from a file handle.
+    """
+    return json.load(file_handle)
 
 
-# List of traces to be plotted.
-plot_data = []
-custom_keys = ["x_str"]
+def process_layout_range(layout, axis):
+    """
+    Ensure that the layout range is specified correctly.
+    """
+    if f'{axis}axis' in layout:
+        if 'range' in layout[f'{axis}axis']:
+            if len(layout[f'{axis}axis']['range']) != 2:
+                layout[f'{axis}axis']['range'] = None
 
-for trace in data:
+
+def map_trace_type_to_plotly_object(trace_type):
+    """
+    Map vsl.plot.TraceType enum to Plotly objects.
+    """
+    type_map = {
+        'scatter': go.Scatter,
+        'pie': go.Pie,
+        'heatmap': go.Heatmap,
+        'surface': go.Surface,
+        'scatter3d': go.Scatter3d,
+        'bar': go.Bar,
+        'histogram': go.Histogram
+    }
+    return type_map[trace_type]
+
+
+def process_trace(trace):
+    """
+    Process a trace to ensure only accepted keys are present.
+    """
+    custom_keys = ["x_str"]
     trace_type = trace.pop("trace_type")
 
-    # Remove all JSON keys not accepted by
-    # Plotly.
-    accepted = dir(type_map[trace_type]) + custom_keys
+    # Remove all JSON keys not accepted by Plotly.
+    accepted = dir(map_trace_type_to_plotly_object(trace_type)) + custom_keys
     keys = list(trace.keys())
     for k in keys:
         if k not in accepted:
             trace.pop(k)
 
-    # oh god why. remove empty keys
-    trace = remove_key(trace, "marker", "size")
-    trace = remove_key(trace, "marker", "color")
-    trace = remove_key(trace, "line", "size")
-    trace = remove_key(trace, "line", "color")
-    trace = remove_key(trace, "line")
-    trace = remove_key(trace, "fill")
-    trace = remove_key(trace, "fillcolor")
-    trace = remove_key(trace, "name")
-    trace = remove_key(trace, "mode")
-    trace = remove_key(trace, "text")
-    trace = remove_key(trace, "textinfo")
-    trace = remove_key(trace, "x")
-    trace = remove_key(trace, "y")
-    trace = remove_key(trace, "z")
+    trace = remove_empty_keys(trace)
 
     if trace_type == 'pie':
         if "marker" in trace:
@@ -117,13 +111,50 @@ for trace in data:
             trace["x"] = trace["x_str"]
         trace.pop("x_str")
 
-    # Since I can only pass [][]f64 to Trace.z,
-    # I must flatten it when dealing with 3D scatters
-    # (and possibly other 3D plots?).
+    # Flatten 'z' when dealing with 3D scatters.
     if trace_type == 'scatter3d':
         trace["z"] = np.array(trace["z"]).flatten()
 
-    plot_data.append(type_map[trace_type](trace))
+    return map_trace_type_to_plotly_object(trace_type)(trace)
 
-fig = go.Figure(data=plot_data, layout=go.Layout(layout))
-fig.show()
+
+def main():
+    parser = argparse.ArgumentParser(description="Run training")
+    parser.add_argument(
+        "--data",
+        dest="data",
+        required=True,
+        help="input file with data",
+        metavar="FILE",
+        type=lambda x: is_valid_file(parser, x),
+    )
+    parser.add_argument(
+        "--layout",
+        dest="layout",
+        required=True,
+        help="input file with layout",
+        metavar="FILE",
+        type=lambda x: is_valid_file(parser, x),
+    )
+
+    args = parser.parse_args()
+
+    # Read data JSON file.
+    data = load_json_file(args.data)
+
+    # Read layout JSON file.
+    layout = load_json_file(args.layout)
+
+    # Ensure correct layout range specification.
+    for axis in ['x', 'y']:
+        process_layout_range(layout, axis)
+
+    # List of traces to be plotted.
+    plot_data = [process_trace(trace) for trace in data]
+
+    fig = go.Figure(data=plot_data, layout=go.Layout(layout))
+    fig.show()
+
+
+if __name__ == "__main__":
+    main()
