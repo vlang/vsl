d8ead599dd63dc49f871cbe385839cc1b53f1eab
ulises-jeremias <ulisescf.24@gmail.com>
2023-10-09 00:23:03 -0300

    Updated vsl.plot setup

--- plot/plot.v
+++ plot/plot.v
@@ -1,31 +1,5 @@
 module plot
 
-import os
-
-const (
-	schema_version = 'v1.0.2'
-	venv_dir_name  = '.plotvenv_${schema_version}'
-	data_dir_name  = '.data_${schema_version}'
-)
-
-// init will ensure that all dependencies are correctly installed and venv initiallized
-fn init() {
-	venv_path := solve_mod_path(plot.venv_dir_name)
-	if !os.is_dir(venv_path) {
-		println('Creating plotly virtualenv...')
-	}
-	init_path := solve_mod_path('scripts', 'create-venv.sh')
-	result := os.execute('bash ${init_path} "${venv_path}"')
-	if result.exit_code != 0 {
-		panic(result.output)
-	}
-	println(result.output)
-}
-
-fn solve_mod_path(dirs ...string) string {
-	return os.join_path(@VMODROOT, ...dirs)
-}
-
 // Plot is the main structure that contains layout and traces
 // to generate plots
 pub struct Plot {
--- plot/show.v
+++ plot/show.v
@@ -5,16 +5,24 @@ import os
 import time
 import vsl.errors
 
+const (
+	schema_version = 'v1.0.3'
+	venv_dir_name  = '.plotvenv_${schema_version}'
+	data_dir_name  = '.data_${schema_version}'
+)
+
 pub fn (p Plot) show() ! {
+	create_venv()!
+
 	ts := time.now().format_ss_micro()
 	plot_str := json.encode_pretty(p.traces)
 
-	venv_path := solve_mod_path(venv_dir_name)
+	venv_path := solve_mod_path(plot.venv_dir_name)
 	if !os.is_dir(venv_path) {
 		return errors.error('plotly virtualenv not found', .efailed)
 	}
 
-	data_dir_path := solve_mod_path(data_dir_name)
+	data_dir_path := solve_mod_path(plot.data_dir_name)
 	if !os.is_dir(data_dir_path) {
 		os.mkdir(data_dir_path) or {
 			return errors.error('could not create needed dir path at ${data_dir_path}.',
@@ -22,8 +30,8 @@ pub fn (p Plot) show() ! {
 		}
 	}
 
-	data_path := solve_mod_path(data_dir_name, 'data-${ts}.json')
-	layout_path := solve_mod_path(data_dir_name, 'layout-${ts}.json')
+	data_path := solve_mod_path(plot.data_dir_name, 'data-${ts}.json')
+	layout_path := solve_mod_path(plot.data_dir_name, 'layout-${ts}.json')
 	run_path := solve_mod_path('scripts', 'run.sh')
 
 	os.write_file(data_path, plot_str) or {
@@ -42,4 +50,21 @@ pub fn (p Plot) show() ! {
 		}
 		println(result.output)
 	}
+}
+
+// create_venv will ensure that all dependencies are correctly installed and venv initiallized
+fn create_venv() ! {
+	venv_path := solve_mod_path(plot.venv_dir_name)
+	if !os.is_dir(venv_path) {
+		println('Creating plotly virtualenv...')
+	}
+	init_path := solve_mod_path('scripts', 'create-venv.sh')
+	result := os.execute('bash ${init_path} "${venv_path}"')
+	if result.exit_code != 0 {
+		return errors.error(result.output, .efailed)
+	}
+}
+
+fn solve_mod_path(dirs ...string) string {
+	return os.join_path(@VMODROOT, ...dirs)
 }
