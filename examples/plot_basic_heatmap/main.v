cbd20b66a95048a8cf2d04c5516cddb747c631a9
Ulises Jeremias <ulisescf.24@gmail.com>
2023-10-15 17:49:42 -0300

    Update main.v

--- examples/plot_basic_heatmap/main.v
+++ examples/plot_basic_heatmap/main.v
@@ -4,14 +4,14 @@ import vsl.plot
 
 z := [[1.0, 0, 30, 50, 1], [20.0, 1, 60, 80, 30], [30.0, 60, 1, -10, 20]]
 x := ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-// y := ['Morning', 'Afternoon', 'Evening']
+y := ['Morning', 'Afternoon', 'Evening']
 
 mut plt := plot.new_plot()
 
 plt.add_trace(
 	trace_type: .heatmap
 	x: x
-	//	y_str: y
+	y: y
 	z: z
 )
 plt.set_layout(
