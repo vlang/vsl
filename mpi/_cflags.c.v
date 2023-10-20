7ae4176f8ab4d9cc2f7c095eb0a8a3e0b3e3e775
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-20 23:35:55 +0200

    Simplify `-l` cflags

--- mpi/_cflags.c.v
+++ mpi/_cflags.c.v
@@ -1,13 +1,14 @@
 module mpi
 
 #flag linux -I/usr/lib/x86_64-linux-gnu/openmpi/include -I/usr/include/x86_64-linux-gnu/mpi -pthread
-#flag linux -pthread -L/usr/lib/x86_64-linux-gnu/openmpi/lib -lmpi
+#flag linux -pthread -L/usr/lib/x86_64-linux-gnu/openmpi/lib
 #flag darwin -I/usr/local/Cellar/open-mpi/4.1.5/include -I/usr/local/include
-#flag darwin -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.1.5/lib -L/usr/local/lib -lmpi
+#flag darwin -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.1.5/lib -L/usr/local/lib
 #flag freebsd -I/usr/local/Cellar/open-mpi/4.1.5/include -I/usr/local/include
-#flag freebsd -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.1.5/lib -L/usr/local/lib -lmpi
+#flag freebsd -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.1.5/lib -L/usr/local/lib
 #flag openbsd -I/usr/local/Cellar/open-mpi/4.1.5/include -I/usr/local/include
-#flag openbsd -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.1.5/lib -L/usr/local/lib -lmpi
+#flag openbsd -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/4.1.5/lib -L/usr/local/lib
 #flag -I@VMODROOT
+#flag -lmpi
 
 #include <cmpi.h>
