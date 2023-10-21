55f8603de1620d50b6d522589439096043a57c60
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-21 02:51:43 +0200

    Fix lapack path on macOS

--- vlas/cflags_notd_cblas.v
+++ vlas/cflags_notd_cblas.v
@@ -4,7 +4,7 @@ module vlas
 #flag linux -L/usr/local/lib -L/usr/lib
 #flag windows -O2
 #flag windows -lgfortran
-#flag darwin -I/usr/local/opt/lapacke/include
-#flag darwin -L/usr/local/opt/lapacke/lib
+#flag darwin -I/usr/local/opt/lapack/include
+#flag darwin -L/usr/local/opt/lapack/lib
 #flag -I@VMODROOT
 #flag -llapacke
