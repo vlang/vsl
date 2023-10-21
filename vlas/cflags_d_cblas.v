904b61dae947ece614a5a0504a36cf295a42d826
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-22 01:18:26 +0200

    Simplify `-l` cflags (#175)
    * Simplify `-l` cflags
    
    * Don't flag blas where not intended to use
    
    * Fix lapack path on macOS

--- vlas/cflags_d_cblas.v
+++ vlas/cflags_d_cblas.v
@@ -1,9 +1,10 @@
 module vlas
 
 #flag linux -O2 -I/usr/local/include -I/usr/lib
-#flag linux -lopenblas -llapacke -L/usr/local/lib -L/usr/lib
+#flag linux -L/usr/local/lib -L/usr/lib
 #flag windows -O2
-#flag windows -lopenblas -lgfortran
+#flag windows -lgfortran
 #flag darwin -I/usr/local/opt/openblas/include
-#flag darwin -llapacke -lopenblas -L/usr/local/opt/openblas/lib
+#flag darwin -L/usr/local/opt/openblas/lib
 #flag -I@VMODROOT
+#flag -lopenblas -llapacke
--- vlas/cflags_notd_cblas.v
+++ vlas/cflags_notd_cblas.v
@@ -1,9 +1,10 @@
 module vlas
 
 #flag linux -O2 -I/usr/local/include -I/usr/lib
-#flag linux -llapacke -L/usr/local/lib -L/usr/lib
+#flag linux -L/usr/local/lib -L/usr/lib
 #flag windows -O2
-#flag windows -lopenblas -lgfortran
-#flag darwin -I/usr/local/opt/openblas/include
-#flag darwin -llapacke -lopenblas -L/usr/local/opt/openblas/lib
+#flag windows -lgfortran
+#flag darwin -I/usr/local/opt/lapack/include
+#flag darwin -L/usr/local/opt/lapack/lib
 #flag -I@VMODROOT
+#flag -llapacke
