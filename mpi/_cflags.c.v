da797a9013dc8af38e826e418d7cb0e52ba6648a
Kim Shrier <gitkim@westryn.net>
2024-01-14 15:26:51 -0700

    update include and lib paths for openmpi

--- mpi/_cflags.c.v
+++ mpi/_cflags.c.v
@@ -2,8 +2,8 @@ module mpi
 
 #flag linux -I/usr/lib/x86_64-linux-gnu/openmpi/include -I/usr/include/x86_64-linux-gnu/mpi -pthread
 #flag linux -pthread -L/usr/lib/x86_64-linux-gnu/openmpi/lib
-#flag freebsd -I/usr/local/include
-#flag freebsd -L/usr/local/opt/libevent/lib -L/usr/local/lib
+#flag freebsd -I/usr/local/mpi/openmpi/include -I/usr/local/include
+#flag freebsd -L/usr/local/mpi/openmpi/lib  -L/usr/local/opt/libevent/lib -L/usr/local/lib
 #flag openbsd -I/usr/local/include
 #flag openbsd -L/usr/local/opt/libevent/lib -L/usr/local/lib
 // Intel, M1 brew, and MacPorts
