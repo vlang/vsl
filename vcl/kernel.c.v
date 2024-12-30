46967a9ccdb9c59cd91b042618ed49183a032261
Peyton Creery <44987569+phcreery@users.noreply.github.com>
2024-12-29 22:46:56 -0600

    fix(vcl): param size (#222)
    * fix vcl param size
    
    * fix vcl param size

--- vcl/kernel.c.v
+++ vcl/kernel.c.v
@@ -107,34 +107,37 @@ fn (k &Kernel) set_args(args ...ArgumentType) ! {
 fn (k &Kernel) set_arg(index int, arg ArgumentType) ! {
 	match arg {
 		u8 {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
-		}
-		f32 {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
-		}
-		f64 {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
-		}
-		i16 {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
-		}
-		i64 {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
 		}
 		i8 {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
-		}
-		int {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
 		}
 		u16 {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
+		}
+		i16 {
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
+		}
+		int {
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
 		}
 		u32 {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
+		}
+		i32 {
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
 		}
 		u64 {
-			return k.set_arg_unsafe(index, int(sizeof(arg)), unsafe { &arg })
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
+		}
+		i64 {
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
+		}
+		f32 {
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
+		}
+		f64 {
+			return k.set_arg_unsafe(index, int(sizeof(*arg)), *arg)
 		}
 		Buffer {
 			return k.set_arg_buffer(index, arg)
