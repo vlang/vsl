d5cc755a1cbdd81cbae49a3c64b3e0705168ca91
Loïc Pottier <loic.pottier@live.fr>
2025-01-20 02:58:39 +0100

    reduce duplication

--- noise/perlin_test.v
+++ noise/perlin_test.v
@@ -3,19 +3,22 @@ module noise
 import rand
 import vsl.float.float64
 
-fn test_perlin_2d() {
+fn setup_generator() !Generator {
 	rand.seed([u32(3155200429), u32(3208395956)])
 	mut gen := Generator.new()
 	gen.randomize()
+	return gen
+}
+
+fn test_perlin_2d() {
+	gen := setup_generator()!
 	result := gen.perlin_2d(0.125, 0.125)
 	expected := 0.4948387311305851
 	assert float64.tolerance(result, expected, 1.0e-6)
 }
 
 fn test_perlin_3d() {
-	rand.seed([u32(3155200429), u32(3208395956)])
-	mut gen := Generator.new()
-	gen.randomize()
+	gen := setup_generator()!
 	result := gen.perlin_3d(0.125, 0.125, 0.125)
 	expected := 0.3713334855776509
 	assert float64.tolerance(result, expected, 1.0e-6)
--- noise/simplex_test.v
+++ noise/simplex_test.v
@@ -3,38 +3,37 @@ module noise
 import rand
 import vsl.float.float64
 
-fn test_simplex_1d() {
+fn setup_generator() !Generator {
 	rand.seed([u32(3155200429), u32(3208395956)])
 	mut gen := Generator.new()
 	gen.randomize()
+	return gen
+}
+
+fn test_simplex_1d() {
+	gen := setup_generator()!
 	result := gen.simplex_1d(0.287)
 	expected := -0.3544283326507284
 	assert float64.tolerance(result, expected, 1.0e-6)
 }
 
 fn test_simplex_2d() {
-	rand.seed([u32(3075200429), u32(3094395956)])
-	mut gen := Generator.new()
-	gen.randomize()
+	gen := setup_generator()!
 	result := gen.simplex_2d(0.287, 0.475)
-	expected := -0.09948661872545192
+	expected := -0.5242242771229899
 	assert float64.tolerance(result, expected, 1.0e-6)
 }
 
 fn test_simplex_3d() {
-	rand.seed([u32(3155200429), u32(3208395956)])
-	mut gen := Generator.new()
-	gen.randomize()
+	gen := setup_generator()!
 	result := gen.simplex_3d(0.287, 0.475, 1.917)
 	expected := -0.06034653476116279
 	assert float64.tolerance(result, expected, 1.0e-6)
 }
 
 fn test_simplex_4d() {
-	rand.seed([u32(3075200429), u32(3094395956)])
-	mut gen := Generator.new()
-	gen.randomize()
+	gen := setup_generator()!
 	result := gen.simplex_4d(0.287, 0.475, 1.917, 0.684)
-	expected := 0.015098415881100141
+	expected := 0.0018411532128437375
 	assert float64.tolerance(result, expected, 1.0e-6)
 }
