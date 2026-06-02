923a6288459dafeed23c327d894de096642a8b31
Alexander Medvednikov <alexander@medvednikov.com>
2026-06-02 05:08:33 +0300

    fix non-vulkan compute stubs

--- compute/context_notd_vulkan.v
+++ compute/context_notd_vulkan.v
@@ -1,9 +1,7 @@
 module compute
 
-import vsl.vulkan
-
 // Stub when not built with `-d vulkan`.
-pub fn (ctx &ComputeContext) resolve_vulkan_device() !&vulkan.Device {
+pub fn (ctx &ComputeContext) resolve_vulkan_device() !voidptr {
 	_ = ctx
 	return error(@FN + ': compile with -d vulkan')
 }
